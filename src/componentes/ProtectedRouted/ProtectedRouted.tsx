1398369f85736a51e5c2d4af6ea2fd184239adff
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-03-11 21:22:37 +0100

    Protected Routes

--- src/componentes/ProtectedRouted/ProtectedRouted.tsx
+++ src/componentes/ProtectedRouted/ProtectedRouted.tsx
@@ -8,13 +8,15 @@ interface User {
 interface Props {
   user: User | null;
   requiredRole?: 'admin' | 'user';
+  children: React.ReactNode; // Los componentes hijos que se renderizarán si el usuario tiene acceso
 }
 
 export const ProtectedRoute = ({ user, requiredRole }: Props) => {
-  if (!user) return <Navigate to="/login" />;
-
+    // si no esta permitido lo enviamos al Login inmediatamente
+  if (!user) return <Navigate to="/login" replace />;
+ //// Usamos replace para que no pueda usar el botón de "Atras" del navegado y volver a la ruta prohibida
   if (requiredRole && user.role !== requiredRole) {
-    return <Navigate to="/denegado" />;
+    return <Navigate to="/denegado" replace />;
   }
 
   return <Outlet />;
